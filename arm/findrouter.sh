8cf40bfb23fa87482ef173cac02ffc0a810dd0d3
gyh <Folov@users.noreply.github.com>
2017-03-16 18:43:32 +0800

    Add files via upload

--- arm/findrouter.sh
+++ arm/findrouter.sh
@@ -1,5 +1,8 @@
-iw dev wlan1 scan >| /tmp/iwscan.txt
-egrep -w '^	SSID|^	signal|^BSS' /tmp/iwscan.txt | sed 's/(on wlan1)//; s/	//; s/BSS //; s/signal: //; s/SSID: //; s/ dBm//; s/ -- associated//; s/^-//' >| /tmp/routerlist.txt
-#grep signal routerlist.txt | sort -k 3 -t : | head -1 | cut -f 1 -d ':' | cat >| bestline.txt
-
-
+while [ 1 ]; do
+	if iw dev wlan1 scan >| /tmp/iwscan.txt 
+	then
+		egrep -w '^	SSID|^	signal|^BSS' /tmp/iwscan.txt | sed 's/(on wlan1)//; s/	//; s/BSS //; s/signal: //; s/SSID: //; s/ dBm//; s/ -- associated//; s/^-//' >| /tmp/routerlist.txt
+		exit 0
+	fi
+	sleep 1
+done
