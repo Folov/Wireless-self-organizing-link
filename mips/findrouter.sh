ca56e7548799ae40d1f150b49e1be4d6420f3a1b
gyh <Folov@users.noreply.github.com>
2017-03-23 21:50:26 +0800

    Delete findrouter.sh

--- mips/findrouter.sh
+++
@@ -1,8 +0,0 @@
-while [ 1 ]; do
-	if iw dev wlan0 scan >| /tmp/iwscan.txt 
-	then
-		egrep -w '^	SSID|^	signal|^BSS' /tmp/iwscan.txt | sed 's/(on wlan0)//; s/	//; s/BSS //; s/signal: //; s/SSID: //; s/ dBm//; s/ -- associated//; s/^-//' >| /tmp/routerlist.txt
-		exit 0
-	fi
-	sleep 1
-done
